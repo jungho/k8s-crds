5434cc1688c27acfdee4f00cb4d9a5fec8334a96
Jungho Kim <jk@architech.ca>
2018-11-30 18:42:10 -0500

    bug in build_push, update .lock

--- website-operator-sdk/build_push.sh
+++ website-operator-sdk/build_push.sh
@@ -5,8 +5,8 @@ operator-sdk build $1
 echo "pushing $1"
 docker push $1
 
-kubectl delete -f ./deploy/crds/example_v1beta1_website_cr.yaml
-kubectl create -f ./deploy/crds/example_v1beta1_website_cr.yaml
+kubectl delete -f ./deploy/crds/example_v1beta1_website_crd.yaml
+kubectl create -f ./deploy/crds/example_v1beta1_website_crd.yaml
 
 kubectl delete -f ./deploy/operator.yaml
 kubectl create -f ./deploy/operator.yaml
